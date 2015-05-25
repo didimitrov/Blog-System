9f7232ac3d02d1e5d18813c6b216c0eef62a95e5
didimitrov <di_dimitrov_@abv.bg>
2015-05-25 14:26:55 +0300

    Created empty projects for Data, Models and Web

---
+++ BlogSys/BlogSys.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogSys.Web.Startup))]
+namespace BlogSys.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
